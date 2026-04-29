b325ef7c53b93b2945cbc0b600b686b17acb3073
It's Trésor <bernardalade92@gmail>
2026-04-29 13:19:55 +0000

    Remove web-admin and server directories from EventHub

--- server/prisma/migrations/20260429000026_add_end_date_and_upcoming_status/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterEnum
-ALTER TYPE "EventStatus" ADD VALUE 'Upcoming';
-
--- AlterTable
-ALTER TABLE "Event" ADD COLUMN     "endDate" TIMESTAMP(3),
-ALTER COLUMN "status" SET DEFAULT 'Upcoming';
